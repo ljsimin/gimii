bdf3184c5bda94b11a10bbc3ba4807a28a883d43
ljubomir.simin <ljubomir.simin@24f01600-ed85-11dd-a1cf-5f4f5b310efa>
2009-03-27 19:21:26 +0000

    Pocetna verzija. Radi, valjda :D

---
+++ trunk/KeepWii/KeepWii/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace KeepWii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "clickMEeee!";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(150, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 22);
+            this.toolStripMenuItem1.Text = "Prikaži prozor";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 22);
+            this.toolStripMenuItem2.Text = "Kraj";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Disconnected";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(142, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(89, 41);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 66);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "KeepWii";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ trunk/KeepWii/KeepWii/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WiimoteLib;
+
+namespace KeepWii
+{
+    public partial class Form1 : Form
+    {
+        Wiimote kontroler = new Wiimote();
+        private delegate void UpdateWiimoteStateDelegate(WiimoteChangedEventArgs args);
+
+        public void Connect() 
+        {
+            kontroler.Connect();
+        if (kontroler.WiimoteState.ExtensionType != ExtensionType.BalanceBoard)
+            kontroler.SetReportType(InputReport.IRExtensionAccel, IRSensitivity.Maximum, true);
+        kontroler.WiimoteChanged += UpdateState;
+        label1.Text = "Connected";
+        }
+
+
+        public void UpdateState(object sender, WiimoteChangedEventArgs args)
+        {
+            try
+            {
+                BeginInvoke(new UpdateWiimoteStateDelegate(UpdateWiimoteChanged), args);
+            }
+            catch (Exception ex)
+            {
+                Application.Exit();
+            }
+        }
+
+        public void UpdateWiimoteChanged(WiimoteChangedEventArgs args)
+        {
+            panel1.BackColor = Color.Black;
+
+            WiimoteState ws = args.WiimoteState;
+
+            if (ws.ButtonState.A || ws.ButtonState.B || ws.ButtonState.Down || ws.ButtonState.Home || ws.ButtonState.Left || ws.ButtonState.Minus || ws.ButtonState.One || ws.ButtonState.Plus || ws.ButtonState.Right || ws.ButtonState.Two || ws.ButtonState.Up)
+                panel1.BackColor = Color.Green;
+
+            if ((Math.Abs(ws.AccelState.Values.X) > 2) || (Math.Abs(ws.AccelState.Values.Y) > 2) || (Math.Abs(ws.AccelState.Values.Z) > 2))
+                panel1.BackColor = Color.LightGreen;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Connect();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
