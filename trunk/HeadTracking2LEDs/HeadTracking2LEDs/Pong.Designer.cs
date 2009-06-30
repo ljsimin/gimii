991be2fb92451383fa0689f7bb3f3f5bb24465fa
n.staletovic <n.staletovic@24f01600-ed85-11dd-a1cf-5f4f5b310efa>
2009-06-30 11:25:43 +0000

    Malo dopunjen Pong (ubrazvanje loptice, game over poruka...)

--- trunk/HeadTracking2LEDs/HeadTracking2LEDs/Pong.Designer.cs
+++ trunk/HeadTracking2LEDs/HeadTracking2LEDs/Pong.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.ClientSize = new System.Drawing.Size(1280, 800);
             this.Text = "Form2";
             this.ResumeLayout(false);
         }
