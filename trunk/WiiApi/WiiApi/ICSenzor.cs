e77c0e664f75235cbfb598a84b498f3e38012d87
pveljko <pveljko@24f01600-ed85-11dd-a1cf-5f4f5b310efa>
2009-09-08 20:03:26 +0000

    Dodata implementacija Emulator-a. Kompajlira se uspesno ali nije jos testirana (doduse bazira se na kodu koji radi sasvim dobro tako da ne bi trebalo da bude ozbiljnih problema). Ispravljene neki sitniji propusti. Dodate neke informacija u ICSenzor.

--- trunk/WiiApi/WiiApi/ICSenzor.cs
+++ trunk/WiiApi/WiiApi/ICSenzor.cs
@@ -12,6 +12,22 @@ namespace WiiApi
     public class ICSenzor
     {
         /// <summary>
+        /// Da li je pronadjen senzor
+        /// </summary>
+        public bool nadjen = false;
+
+        /// <summary>
+        /// Velicina pronadjenog senzora. Vrednosti su od 0 do 15. Retko se javlja kao bitan faktor.
+        /// </summary>
+        private int velicina = 0;
+
+        public int Velicina
+        {
+            get { return velicina; }
+            set { velicina = value; }
+        }
+
+        /// <summary>
         /// Stanje po X osi
         /// </summary>
         public float x = 0;
@@ -33,12 +49,23 @@ namespace WiiApi
             set { y = value; }
         }
 
+        public bool Nadjen
+        {
+            get { return nadjen; }
+            set { value = nadjen; }
+        }
+
         /// <summary>
         /// Konstruktor koji vrsi inicijalizaciju
         /// </summary>
         /// <param name="x">vrednost po X osi</param>
         /// <param name="y">vrednost po Y osi</param>
-        public ICSenzor(float x, float y) { X = x; Y = y; }
+        /// <param name="nadjen">da li je senzor nadjen</param>
+        public ICSenzor(float x, float y, Boolean nadjen) 
+        { 
+            X = x; 
+            Y = y; this.nadjen = nadjen; 
+        }
 
         /// <summary>
         /// Podrazumevani konstruktor
