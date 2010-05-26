6e11c4ffc8d3d5a87c802afc8830d144d807d347
pveljko <pveljko@24f01600-ed85-11dd-a1cf-5f4f5b310efa>
2010-05-26 19:14:22 +0000

    Dodata posebna pod-biblioteka za 3D tracking.

---
+++ GimiiIntegrated/WiiApi.Tracking3d/Vector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WiiApi.Tracking3d
+{
+    public struct Vector
+    {
+        private float _x;
+        private float _y;
+        private float _z;
+        public Vector(float x, float y, float z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+        public float Z
+        {
+            get
+            {
+                return _z;
+            }
+            set
+            {
+                _z = value;
+            }
+        }
+    }
+}
