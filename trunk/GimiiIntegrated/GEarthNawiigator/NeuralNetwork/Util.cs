922f8af2f03a109b8b8131c05e2e57e02ce13fb6
nemanjanedic@gmail.com <nemanjanedic@gmail.com@24f01600-ed85-11dd-a1cf-5f4f5b310efa>
2010-06-05 21:06:59 +0000

    Ubacen kod za RBF neuronske mreze.

---
+++ trunk/GimiiIntegrated/GEarthNawiigator/NeuralNetwork/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GEarthNawiigator.NeuralNetwork
+{
+    public class Util
+    {
+
+        internal static double sigmoid(double alpha, double x)
+        {
+            return 1 / (1 + Math.Exp(-alpha * x));
+        }
+
+        internal static double gaussian(double lambda, double[] x, double[] c)
+        {
+            double diff = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                diff += Math.Pow(x[i] - c[i], 2);
+            }
+            return Math.Exp(-lambda * diff);
+        }
+    }
+}
